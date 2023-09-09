25654b2f88d1148f115597011187b4734b929156
Chunghee Hwang <hch0821@gmail.com>
2023-09-09 14:48:17 +0900

    refactor: loggin aspect 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,44 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.3"
-	id("io.spring.dependency-management") version "1.1.3"
+  java
+  id("org.springframework.boot") version "3.1.3"
+  id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.chung"
 version = "1.0.0"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+  sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-redis")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+  implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+  implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+  implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+  implementation("org.springframework.boot:spring-boot-starter-aop")
+  compileOnly("org.projectlombok:lombok")
+  runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+  annotationProcessor("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
---
+++ src/main/java/com/chung/lifusic/account/aspect/LoggingAspect.java
@@ -0,0 +1,28 @@
+package com.chung.lifusic.account.aspect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+    @AfterThrowing(pointcut = "execution(* com.chung.lifusic.account.controller.*.*(..))", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("Controller error:: {}, signature: {}, target: {}", e.getMessage(), joinPoint.getSignature().getName(), joinPoint.getTarget());
+    }
+
+    @Around("execution(* com.chung.lifusic.account.controller.*.*(..))")
+    public Object controllerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Controller before:: signature: {}, target: {}", joinPoint.getSignature().getName(), joinPoint.getTarget());
+        Object result = joinPoint.proceed();
+        log.info("Controller after:: signature: {}, target: {}", joinPoint.getSignature().getName(), joinPoint.getTarget());
+        return result;
+    }
+}
