bb2860af73048364c15bb1aa3d90cf2c06ddc081
Chunghee Hwang <hch0821@gmail.com>
2023-09-27 23:54:41 +0900

    feat: health check api

---
+++ src/main/java/com/chung/lifusic/account/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.chung.lifusic.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String hello() {
+        return "ok";
+    }
+}
+
